5a2a0dd82a5b5af29aa5865867d4a55401e6f184
Archie Watson Irvine <157597766+ArchieWatsonIrvine@users.noreply.github.com>
2026-01-08 17:23:15 +0000

    Replace hardcoded API key with placeholder

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const apikey = "46F0B613-A331-42CE-B9EF-0096BB1F3547";
+  const apikey = "[APIKEY]";
   const cloneReq = req.clone({
     setHeaders: {
       api_key: apikey,
